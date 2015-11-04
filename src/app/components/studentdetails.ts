ccaf70708edee09bf94c8a6a9468524767ae799d
John Gorter <John.Gorter@gmail.com>
2015-11-04 16:38:20 +0100

    Components_solution

--- src/app/components/studentdetails.ts
+++ src/app/components/studentdetails.ts
@@ -1,4 +1,25 @@
 // YOUR IMPORTS HERE...
+import {Component, EventEmitter, NgClass} from 'angular2/angular2';
+import {student} from '../models/student';
 
 
 // YOUR COMPONENT ANNOTATION AND CLASS HERE...
+@Component({
+	selector:'studentdetails',
+	inputs:['student','isselected'],
+	outputs:['selected'],
+	directives:[NgClass],
+	template:`
+	<div (click)="setSelected()" [ng-class]="{defaultPrimaryColor:isselected, student:true}" >{{ student.firstname }} {{ student.lastname }} {{ student.school }}</div>
+	`
+})
+export class StudentDetails
+{
+	student:student;
+	isselected:boolean;
+	selected:EventEmitter = new EventEmitter();
+	 
+	setSelected(){
+		this.selected.next(this.student); 
+	}
+}
