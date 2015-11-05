36b56a4c31753901af77322b98382883ccbce39d
John Gorter <John.Gorter@gmail.com>
2015-11-05 21:21:20 +0100

    Forms_solution

--- src/app/form.js
+++ src/app/form.js
@@ -1,4 +1,4 @@
-System.register(["angular2/angular2", './services/studenttrackservice', './services/studentservice'], function(exports_1) {
+System.register(["angular2/angular2", './services/studenttrackservice', './services/studentservice', './pipes/studentformatter'], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
         switch (arguments.length) {
@@ -10,7 +10,7 @@ System.register(["angular2/angular2", './services/studenttrackservice', './servi
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var angular2_1, angular2_2, studenttrackservice_1, studentservice_1;
+    var angular2_1, angular2_2, studenttrackservice_1, studentservice_1, studentformatter_1;
     var DemoFormSku;
     return {
         setters:[
@@ -23,6 +23,9 @@ System.register(["angular2/angular2", './services/studenttrackservice', './servi
             },
             function (studentservice_1_1) {
                 studentservice_1 = studentservice_1_1;
+            },
+            function (studentformatter_1_1) {
+                studentformatter_1 = studentformatter_1_1;
             }],
         execute: function() {
             DemoFormSku = (function () {
@@ -32,17 +35,21 @@ System.register(["angular2/angular2", './services/studenttrackservice', './servi
                 }
                 DemoFormSku.prototype.onSubmitFormA = function (form) {
                     if (form.valid) {
+                        this.studenttrackservice.addStudentTrack(form.value.trackname);
                     }
                 };
                 DemoFormSku.prototype.onSubmitFormB = function (form) {
                     if (form.valid) {
+                        var track = this.studenttrackservice.getStudentTrackByName(form.value.track);
+                        track.addStudentToTrack(this.studentservice.getStudentById(form.value.student));
                     }
                 };
                 DemoFormSku = __decorate([
                     angular2_1.Component({
                         selector: 'demo-form-sku',
                         directives: [angular2_2.CORE_DIRECTIVES, angular2_2.FORM_DIRECTIVES],
-                        template: "\n    <div class=\"trackform light-primary-color text-primary-color\"><h1 class=\"dark-primary-color text-primary-color\">Add studenttrack or student to track</h1>\n    <br/>\n    <form ... style=\"display:inline;margin-left:50px;\">\n    <span class=\"form-group\">\n      <label for=\"trackname\">Add a new track</label>\n      <input type=\"text\" class=\"form-control\" id=\"trackName\" placeholder=\"Track Name\" ... required />\n       <button type=\"submit\" class=\"btn btn-default\" ... >Add</button>\n    </span>\n   \n    </form>\n    <form ... style=\"float:right;margin-right:100px;\">\n    <span class=\"form-group\">\n      <label for=\"student\">Add Student</label>\n      <select name=\"student\" id=\"student\" ng-control=\"student\">\n        <option value=\"\">-select a student-</option>\n        <option *ng-for=\"#student of studentservice.getStudents()\" ...>...</option>\n      </select>\n      <label for=\"track\">to track</label>\n     <select name=\"track\" id=\"student\" ng-control=\"track\" required>\n        <option value=\"\" selected>-select a track-</option>\n        <option *ng-for=\"#track of studenttrackservice.getStudentTracks()\" ...>....</option>\n      </select>\n       <button type=\"submit\" class=\"btn btn-default\" ...>Add</button>\n    </span>\n    </form>\n    <br/><br/>\n    </div>\n  ",
+                        pipes: [studentformatter_1.StudentFormatter],
+                        template: "\n    <div class=\"trackform light-primary-color text-primary-color\"><h1 class=\"dark-primary-color text-primary-color\">Add studenttrack or student to track</h1>\n    <br/>\n    <form #fa=\"form\" (submit)=\"onSubmitFormA(fa)\" style=\"display:inline;margin-left:50px;\">\n    <span class=\"form-group\">\n      <label for=\"trackname\">Add a new track</label>\n      <input type=\"text\" class=\"form-control\" id=\"trackName\" placeholder=\"Track Name\" ng-control=\"trackname\" required />\n       <button type=\"submit\" class=\"btn btn-default\" [disabled]=\"!fa.valid\">Add</button>\n    </span>\n   \n    </form>\n    <form #fb=\"form\" (submit)=\"onSubmitFormB(fb)\" style=\"float:right;margin-right:100px;\">\n    <span class=\"form-group\">\n      <label for=\"student\">Add Student</label>\n      <select name=\"student\" id=\"student\" ng-control=\"student\">\n        <option value=\"\">-select a student-</option>\n        <option *ng-for=\"#student of studentservice.getStudents()\" [value]=\"student.id\">{{ student.firstname }}</option>\n      </select>\n      <label for=\"track\">to track</label>\n     <select name=\"track\" id=\"student\" ng-control=\"track\" required>\n        <option value=\"\" selected>-select a track-</option>\n        <option *ng-for=\"#track of studenttrackservice.getStudentTracks()\" [value]=\"track.name\">{{ track.name }}</option>\n      </select>\n       <button type=\"submit\" class=\"btn btn-default\" [disabled]=\"!fb.valid\">Add</button>\n    </span>\n    </form>\n    <br/><br/>\n    </div>\n  ",
                         styles: ["\n\t .trackform { border:1px solid black;margin:5px;padding:0px; }\n\t .trackform h1 { margin:0px;padding:15px;}\n\t"]
                     }), 
                     __metadata('design:paramtypes', [studentservice_1.StudentService, studenttrackservice_1.StudentTrackService])
--- src/app/form.ts
+++ src/app/form.ts
@@ -2,34 +2,36 @@ import {Component, bootstrap, View} from "angular2/angular2";  
 import {FORM_DIRECTIVES, FormBuilder, ControlGroup, CORE_DIRECTIVES} from "angular2/angular2";
 import { StudentTrackService } from './services/studenttrackservice';
 import { StudentService } from './services/studentservice';
+import { StudentFormatter } from './pipes/studentformatter';
 
 @Component({  
   selector: 'demo-form-sku',
   directives: [CORE_DIRECTIVES, FORM_DIRECTIVES],  
+  pipes:[StudentFormatter],
   template: `
     <div class="trackform light-primary-color text-primary-color"><h1 class="dark-primary-color text-primary-color">Add studenttrack or student to track</h1>
     <br/>
-    <form ... style="display:inline;margin-left:50px;">
+    <form #fa="form" (submit)="onSubmitFormA(fa)" style="display:inline;margin-left:50px;">
     <span class="form-group">
       <label for="trackname">Add a new track</label>
-      <input type="text" class="form-control" id="trackName" placeholder="Track Name" ... required />
-       <button type="submit" class="btn btn-default" ... >Add</button>
+      <input type="text" class="form-control" id="trackName" placeholder="Track Name" ng-control="trackname" required />
+       <button type="submit" class="btn btn-default" [disabled]="!fa.valid">Add</button>
     </span>
    
     </form>
-    <form ... style="float:right;margin-right:100px;">
+    <form #fb="form" (submit)="onSubmitFormB(fb)" style="float:right;margin-right:100px;">
     <span class="form-group">
       <label for="student">Add Student</label>
       <select name="student" id="student" ng-control="student">
         <option value="">-select a student-</option>
-        <option *ng-for="#student of studentservice.getStudents()" ...>...</option>
+        <option *ng-for="#student of studentservice.getStudents()" [value]="student.id">{{ student.firstname }}</option>
       </select>
       <label for="track">to track</label>
      <select name="track" id="student" ng-control="track" required>
         <option value="" selected>-select a track-</option>
-        <option *ng-for="#track of studenttrackservice.getStudentTracks()" ...>....</option>
+        <option *ng-for="#track of studenttrackservice.getStudentTracks()" [value]="track.name">{{ track.name }}</option>
       </select>
-       <button type="submit" class="btn btn-default" ...>Add</button>
+       <button type="submit" class="btn btn-default" [disabled]="!fb.valid">Add</button>
     </span>
     </form>
     <br/><br/>
@@ -50,12 +52,14 @@ export class DemoFormSku {  
       }
       onSubmitFormA(form){
          if (form.valid) {
-          // add a studenttrack to the studenttrack list
+           
+          this.studenttrackservice.addStudentTrack(form.value.trackname); 
         }
       }
       onSubmitFormB(form) { 
         if (form.valid) {
-          // add the selected student to the selected studenttrack
+          var track = this.studenttrackservice.getStudentTrackByName(form.value.track)
+          track.addStudentToTrack(this.studentservice.getStudentById(form.value.student)); 
         }
       }
 }
